ad750dc37573e5c786456a53438a63e6ec37cf81
Ruhulcse <ruhul.cse7862@gmail.com>
2021-02-25 13:50:44 +0600

    user create testing

--- backend/tests/userTest.js
+++ backend/tests/userTest.js
@@ -3,17 +3,30 @@ import chaiHttp from "chai-http";
 import app from "../server.js";
 chai.use(chaiHttp);
 chai.should();
+
+//user create route testing
 describe("user api testing", () => {
   describe("POST /users", () => {
-    it("user create with success message", (done) => {
+    it("user create with success message", function (done) {
+      this.timeout(10000);
+      let user = {
+        name: "Ruhul amin",
+        email: "ruhul1aad1@gmail.com",
+        password: "123456",
+        isAdmin: "false",
+      };
       chai
         .request(app)
         .post("/api/users")
+        .send(user)
         .end((err, res) => {
+          console.log(res.body);
           res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success");
+          res.body.should.have.property("body");
           done();
         });
     });
   });
 });
-console.log("testing");
