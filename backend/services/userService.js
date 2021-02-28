b4ce2cc06388309c14a9a76d30b7bdd53ed50c87
Ruhulcse <ruhul.cse7862@gmail.com>
2021-02-28 15:57:18 +0600

    User create API written

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -3,6 +3,11 @@ import User from "../models/userModel.js";
 const addUser = async (data) => {
   const { name, email, isAdmin, password } = data;
   try {
+    // const userexist = await User.find({ email });
+    // if (userexist) {
+    //   res.status(400);
+    //   throw new Error("This email already used");
+    // }
     const result = await User.create({
       name,
       email,
